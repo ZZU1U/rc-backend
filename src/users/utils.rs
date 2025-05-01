8228fb4f580939c205f25ab1ccd99438a912789a
Gleb Anohin <dudu@dadoot.local>
2025-05-01 13:22:37 +0300

    better default admin handling

--- src/users/utils.rs
+++ src/users/utils.rs
@@ -8,6 +8,26 @@ pub async fn check_default_admin(state: AppState) {
     let admin_email = env::var("DEFAULT_ADMIN_EMAIL").expect("Admin email is not specified");
     let admin_pwd = env::var("DEFAULT_ADMIN_PASSWORD").expect("Admin password is not specified");
 
+    let existing_admin = sqlx::query_as!(
+        User,
+        r#"
+        SELECT * FROM "user"
+        WHERE (email = $1)
+        "#,
+        admin_email
+    )
+        .fetch_one(&state.pool).await;
+
+    match existing_admin {
+        Ok(admin) => {
+            println!("Existing admin {:?}", admin);
+            return;
+        },
+        Err(err) => {
+            println!("No admin or something went wrong");
+        }
+    };
+
     let admin_hash = hash_password(admin_pwd).await.unwrap();
     
     let admin = sqlx::query_as!(
@@ -15,12 +35,11 @@ pub async fn check_default_admin(state: AppState) {
         r#"
         INSERT INTO "user" (id, email, password_hash, is_verified, is_super)
         VALUES ($1, $2, $3, true, true)
-        ON CONFLICT (email) DO NOTHING
         RETURNING *
         "#,
         uuid::uuid!("019688b5-075d-73d1-99fe-1708ac57bdd8"), admin_email, admin_hash
     )
         .fetch_one(&state.pool).await;
 
-    println!("Default admin is {:?}", admin);
+    println!("New default admin is {:?}", admin);
 }
